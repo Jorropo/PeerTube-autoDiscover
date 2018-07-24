2c2850c10345c6d3350c800cd78edfb6ae5beb45
Jorropo <jorropo.pgm@gmail.com>
2018-07-24 20:38:09 +0200

    Try all knowed node

--- updateInstances.joinpeertube.org.py
+++ updateInstances.joinpeertube.org.py
@@ -23,11 +23,11 @@ try: #try for don't crash on ctrl + C
         searchIng = toScan.pop(0)
         try: #try for don't crash on urllib fail
             for i in json.loads(urlopen(Request("https://"+searchIng+"/api/v1/server/following?count="+str(json.loads(urlopen(Request("https://"+searchIng+"/api/v1/server/following?count=0"), timeout=15).read().decode())["total"])), timeout=15).read().decode())["data"]:
-                if i["following"]["host"] not in allNode and i["state"] == "accepted":
+                if i["following"]["host"] not in allNode:
                     allNode.append(i["following"]["host"])
                     toScan.append(i["following"]["host"])
             for i in json.loads(urlopen(Request("https://"+searchIng+"/api/v1/server/followers?count="+str(json.loads(urlopen(Request("https://"+searchIng+"/api/v1/server/followers?count=0"), timeout=15).read().decode())["total"])), timeout=15).read().decode())["data"]:
-                if i["followers"]["host"] not in allNode and i["state"] == "accepted":
+                if i["followers"]["host"] not in allNode:
                     allNode.append(i["followers"]["host"])
                     toScan.append(i["followers"]["host"])
         except KeyboardInterrupt:
