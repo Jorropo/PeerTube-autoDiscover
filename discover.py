fd25327cac8a1658fe2144121c8933a13db3118a
Jorropo <jorropo.pgm@gmail.com>
2018-07-24 16:56:03 +0200

    Create discover.py

---
+++ discover.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import json
+import sys
+import os
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+toScan = []
+
+if len(sys.argv) != 1:
+    print("Wrong argument ammount, give one argument, entry node without https")
+    os.exit(2)
+
+toScan.append(sys.argv[1])
+allNode = toScan.copy()
+
+try: #try for don't crash on ctrl + C
+    while len(toScan) > 0:
+        try: #try for don't crash on urllib fail
+            searchIng = toScan.pop([0])
+            for i in json.loads(urlopen(Request("https://"+searchIng+"/api/v1/server/following")))["data"]:
+                if not i in allNode:
+                    allNode.append(i["following"]["host"])
+                    toScan.append(i["following"]["host"])
+                    print("Discovered : " + i["following"]["host"])
+        except:
+            pass
+except:
+    print("canceled")
+
+for i in allNode:
+    print(i)
