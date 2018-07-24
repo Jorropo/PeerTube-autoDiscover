8f0e5eadacf5d3a0ec116c662f57766bbee61d86
Jorropo <jorropo.pgm@gmail.com>
2018-07-24 18:38:18 +0200

    Make more scriptable

--- discover.py
+++ discover.py
@@ -8,7 +8,7 @@ toScan = []
 allNode = []
 
 if len(sys.argv) != 2:
-    print("Wrong argument ammount, give one argument, entry node without https")
+    sys.stderr.write("Wrong argument ammount, give one argument, entry node without https")
     sys.exit(2)
 
 toScan.append(sys.argv[1])
@@ -16,21 +16,16 @@ allNode.append(sys.argv[1])
 
 try: #try for don't crash on ctrl + C
     while len(toScan) > 0:
-        print("Staying to scan : " + str(len(toScan)))
         try: #try for don't crash on urllib fail
             searchIng = toScan.pop(0)
-            print("Searching : " + searchIng)
             for i in json.loads(urlopen(Request("https://"+searchIng+"/api/v1/server/following?count="+str(json.loads(urlopen(Request("https://"+searchIng+"/api/v1/server/following?count=0"), timeout=15).read().decode())["total"])), timeout=15).read().decode())["data"]:
                 if i["following"]["host"] not in allNode:
                     allNode.append(i["following"]["host"])
                     toScan.append(i["following"]["host"])
-                    print("Discovered : " + i["following"]["host"])
+                    sys.stdout.write(i["following"]["host"]+"\n")
+        except KeyboardInterrupt:
+            raise Exception('Pass out this error.')
         except:
-            pass
+            sys.stderr.write("error on contacting a node")
 except:
-    print("canceled")
-
-print("All nodes lists : (" + str(len(allNode)) + " nodes finded)")
-
-for i in allNode:
-    print(i)
+    sys.stderr.write("canceled")
